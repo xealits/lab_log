e6890c0dd692010fdf9bb8bc577354d641f81059
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2020-02-26 16:28:28 +0000

    Made a better conversion to xml...
    xml is a pain
    
    	new file:   dsets_info_3.xml
    	new file:   dsets_info_convert.py

---
+++ dsets_info_convert.py
@@ -0,0 +1,36 @@
+import yaml
+
+yaml_file = 'dsets_info_2.yaml'
+
+with open(yaml_file) as f:
+    d = yaml.load(f)
+
+# convert to xml
+#def print_xml_node(name, content="", attributes={}, indent=0):
+#    print(' '*indent + '<{name} {attrs}>')
+
+def xml_value(val):
+    value = str(val)
+    try:
+        value = str(float(value))
+    except:
+        value = '"%s"' % value
+    return value
+
+indent = ' '*2
+print('<hard_processes>')
+for hard_proc in d['hard_processes']:
+
+    attributes = []
+    for k, v in hard_proc.items():
+        if k != 'dsets':
+            value = xml_value(v)
+            attributes.append('='.join((k, value)))
+    print(indent + '<hard_process %s>' % ' '.join(attributes))
+    for dset in hard_proc['dsets']:
+        dset_attr = '<dset dtag={dtag} logic_path={lp}>'.format(
+                dtag = xml_value(dset['dtag']),
+                lp   = xml_value(dset['logic_path']))
+        print(indent*2 + dset_attr)
+
+print('</hard_processes>')
